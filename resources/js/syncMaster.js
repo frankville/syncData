d60918f336386cd7974d1aa4fd9dd2be9c5d8d7e
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-05-07 02:43:02 -0300

    changed to heroku

--- resources/js/syncMaster.js
+++ resources/js/syncMaster.js
@@ -1,6 +1,6 @@
   var socket ;
   $(document).ready(function(){
-	socket = io.connect('http://localhost/');
+	socket = io.connect('http://syncdata.herokuapp.com/');
   	socket.on('news', function (data){
     console.log(data);
     socket.emit('my other event', { my: 'data' });
