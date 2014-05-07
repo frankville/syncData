002a97370c3c9ca37c94d4f62df2e6a4684a8a4b
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-05-07 00:23:39 -0300

    algo

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ var express = require('express'),
   , io = require('socket.io').listen(server);
 
 
-
+console.log("antes del listen");
 server.listen(80);
-
+console.log("desp del listen");
 app.use('/',express.static(__dirname +'/syncDev/'));
 app.get('/', function(request, response) {
+	console.log("entra aca");
   response.send(__dirname);
 });
 
 
 
 io.sockets.on('connection', function (socket) {
+	console.log("llega a onconnection?");
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
