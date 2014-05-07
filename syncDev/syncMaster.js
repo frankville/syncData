fc7566e11cad6a2f40b004e0c45da954790cac1d
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-05-07 01:02:50 -0300

    asdasd

--- syncDev/syncMaster.js
+++ syncDev/syncMaster.js
@@ -1,7 +1,7 @@
   var socket ;
   $(document).ready(function(){
 	socket = io.connect('http://syncdata.herokuapp.com');
-  	socket.on('news', function (data) {
+  	socket.on('news', function (data){
     console.log(data);
     socket.emit('my other event', { my: 'data' });
   });
